40173effbbe4c1ce83c1a23df539d68e83b595c6
krzysztofszczybylo <s218037@student.uek.krakow.pl>
2023-03-26 19:52:04 +0200

    25th commit

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
-var cacheName = 'PWA';
+var cacheName = 'hello-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './image/image.png',
-  './js/app.js'
+  './js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
