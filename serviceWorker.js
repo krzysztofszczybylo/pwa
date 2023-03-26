bc307f5479f0a2eeb59a7aacad5aab9c626690d1
krzysztofszczybylo <s218037@student.uek.krakow.pl>
2023-03-26 19:28:33 +0200

    twentyth commit

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,17 +1,25 @@
-let CACHE_NAME = 'cache';
-let urlsToCache = [
-    'image.png',
-    'index.html'
+var cacheName = 'PWA';
+var filesToCache = [
+  './',
+  './index.html',
+  './image.png',
+  './app.js'
+];
 
-    ];
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
 
-self.addEventListener('install', function(event) {
-// Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-        })
-    );
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
